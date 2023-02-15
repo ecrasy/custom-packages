4a84a9e8e7da98ffef201cd8d7d53ed044d6ff9e
Carbon <ecrasy@gmail.com>
2023-02-15 19:45:30 +0800

    update luci app qbittorrent

--- luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
+++ luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
@@ -5,14 +5,14 @@ function index()
 		return
 	end
 
-	entry({"admin", "nas", "qBittorrent"}, alias("admin", "nas", "qBittorrent", "basic"), _("qBittorrent"), 30).dependent = true
-	entry({"admin", "nas", "qBittorrent", "basic"}, cbi("qbittorrent/basic"), _("Basic Settings"), 1).leaf = true
-	entry({"admin", "nas", "qBittorrent", "connection"}, cbi("qbittorrent/connection"), _("Connection Settings"), 2).leaf = true
-	entry({"admin", "nas", "qBittorrent", "downloads"}, cbi("qbittorrent/downloads"), _("Download Settings"), 3).leaf = true
-	entry({"admin", "nas", "qBittorrent", "bittorrent"}, cbi("qbittorrent/bittorrent"), _("Bittorrent Settings"), 4).leaf = true
-	entry({"admin", "nas", "qBittorrent", "webgui"}, cbi("qbittorrent/webgui"), _("WebUI Settings"), 5).leaf = true
-	entry({"admin", "nas", "qBittorrent", "advanced"}, cbi("qbittorrent/advanced"), _("Advance Settings"), 6).leaf = true
-	entry({"admin", "nas", "qBittorrent", "status"}, call("act_status")).leaf = true
+	entry({"admin", "services", "qBittorrent"}, alias("admin", "services", "qBittorrent", "basic"), _("qBittorrent"), 30).dependent = true
+	entry({"admin", "services", "qBittorrent", "basic"}, cbi("qbittorrent/basic"), _("Basic Settings"), 1).leaf = true
+	entry({"admin", "services", "qBittorrent", "connection"}, cbi("qbittorrent/connection"), _("Connection Settings"), 2).leaf = true
+	entry({"admin", "services", "qBittorrent", "downloads"}, cbi("qbittorrent/downloads"), _("Download Settings"), 3).leaf = true
+	entry({"admin", "services", "qBittorrent", "bittorrent"}, cbi("qbittorrent/bittorrent"), _("Bittorrent Settings"), 4).leaf = true
+	entry({"admin", "services", "qBittorrent", "webgui"}, cbi("qbittorrent/webgui"), _("WebUI Settings"), 5).leaf = true
+	entry({"admin", "services", "qBittorrent", "advanced"}, cbi("qbittorrent/advanced"), _("Advance Settings"), 6).leaf = true
+	entry({"admin", "services", "qBittorrent", "status"}, call("act_status")).leaf = true
 end
 
 function act_status()
